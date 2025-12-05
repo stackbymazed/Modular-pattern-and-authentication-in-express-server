7da56d1a5b53dd0ce43dffed35385a42ca40845c
Nayem783982 <majedulislam223311@gmail.com>
2025-12-05 16:40:55 +0600

    created simple server and setup folder structure

---
+++ src/server.ts
@@ -0,0 +1,119 @@
+import express, { Request, Response } from 'express'
+import { Pool } from "pg"
+import config from './config'
+import { initDB } from './config/db'
+
+const app = express()
+const port = config.Port
+
+//middleware for the body data parse
+app.use(express.json())
+
+//table data er jonno
+app.use(express.urlencoded())
+
+//initialize database
+initDB()
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('server is Running port 3000')
+})
+
+//users CURD
+app.post("/users", async (req: Request, res: Response) => {
+    const { name, email } = req.body;
+    try {
+        const result = await pool.query(`INSERT INTO users(name,email) VALUES($1,$2) RETURNING *`, [name, email]);
+        console.log(result.rows[0]);
+        res.status(200).json({
+            success: true,
+            message: "Data inserted successfully.....!"
+        })
+
+    } catch (err: any) {
+        res.status(404).json({
+            success: false,
+            message: err.message
+        })
+    }
+})
+
+app.get("/users", async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query(`SELECT * FROM users`);
+        res.status(200).json({
+            success: true,
+            message: "Data is having successfully......!",
+            data: result.rows
+        })
+    } catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })
+    }
+})
+
+app.get("/users/:id", async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [req.params.id])
+        res.status(200).json({
+            success: true,
+            message: "Data having successfully.....!",
+            data: result.rows
+        })
+    } catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })
+    }
+})
+
+app.put("/users/:id", async (req: Request, res: Response) => {
+    const { name, email } = req.body;
+    console.log(name, email);
+    console.log(req.params.id);
+    try {
+        const result = await pool.query(`UPDATE users SET name=$1, email = $2 WHERE id = $3 RETURNING *`, [name, email, req.params.id])
+        console.log(result);
+        if (result.rows.length === 0) {
+            res.status(404).json({
+                success: true,
+                message: "No data pound there.....!"
+            })
+        } else {
+            res.status(200).json({
+                success: true,
+                message: "Data updated successfully .....!",
+                data: result.rows
+            })
+        }
+    } catch (err: any) {
+        res.status(404).json({
+            success: false,
+            message: "content not pound"
+        })
+    }
+})
+
+app.delete("/users/:id", async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query(`DELETE FROM users WHERE id = $1`, [req.params.id])
+        res.status(200).json({
+            success: true,
+            message: "Data delete successfully........!",
+            data: result.rowCount
+        })
+        console.log(result);
+    } catch (err) {
+        res.status(404).json({
+            success: false,
+            message: "Data Not pound"
+        })
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
